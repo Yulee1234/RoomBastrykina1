b935143d1b8ed0c620898c056b1d55d013c1b798
Юлия Виноградова <yukki22843@gmail.com>
2021-10-28 14:14:14 +0300

    Добавьте файлы проекта.

---
+++ RoomBastrykina1/Admin/AdminAddProduct.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoomBastrykina1.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminAddProduct.xaml
+    /// </summary>
+    public partial class AdminAddProduct : Window
+    {
+        public AdminAddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void txbNameProd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbCategoryChois_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txbPriceChois_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnObzor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txbDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txbMaterial_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
